3e3480f9bdfeeccc3b65c336739f1a403a35221c
Akshat Oke <Akshat.Oke@amd.com>
2025-05-19 11:02:19 +0000

    Avoid MMI in MIRPrinter

--- llvm/include/llvm/CodeGen/MIRPrinter.h
+++ llvm/include/llvm/CodeGen/MIRPrinter.h
@@ -24,6 +24,7 @@ class MachineBasicBlock;
 class MachineFunction;
 class MachineModuleInfo;
 class Module;
+
 template <typename T> class SmallVectorImpl;
 
 class PrintMIRPreparePass : public PassInfoMixin<PrintMIRPreparePass> {
@@ -50,8 +51,8 @@ LLVM_ABI void printMIR(raw_ostream &OS, const Module &M);
 
 /// Print a machine function using the MIR serialization format to the given
 /// output stream.
-LLVM_ABI void printMIR(raw_ostream &OS, const MachineModuleInfo &MMI,
-                       const MachineFunction &MF);
+LLVM_ABI void printMIR(raw_ostream &OS, MachineModuleInfo *MMI,
+              FunctionAnalysisManager *MFA, const MachineFunction &MF);
 
 /// Determine a possible list of successors of a basic block based on the
 /// basic block machine operand being used inside the block. This should give
