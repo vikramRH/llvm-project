6e3ced230d9317efbfc3900053e03b547189fe10
Prasoon Mishra <Prasoon.Mishra@amd.com>
2025-10-16 17:55:42 +0000

    Fixes to be upstreamed - Improve PhiElimination and LCSSA Pass Behavior
    1. Added MachineLoopInfo in PhiElimination pass for legacy pipeline to
       improve edge splitting decisions when available.
    2. Removed required api from the LCSSA pass to prevent its execution when
       optnone is specified, aligning with expected behavior.

--- llvm/include/llvm/Transforms/Utils/LCSSA.h
+++ llvm/include/llvm/Transforms/Utils/LCSSA.h
@@ -38,7 +38,6 @@ namespace llvm {
 class LCSSAPass : public PassInfoMixin<LCSSAPass> {
 public:
   LLVM_ABI PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
-  static bool isRequired() { return true; }
 };
 } // end namespace llvm
 
